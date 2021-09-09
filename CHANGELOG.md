f55206dfe9498b63bd1f5f10e1e725006ced0928
Dmitry Silchenko <dmytro.silchenko@justrelate.com>
2021-09-09 17:37:41 +0200

    Add SSM support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 ## Changelog
 
+#### 1.1.0
+- ssm support added `<%= ssm["/#{stage}/app/application/db_host"] %>`
+
 #### 1.0.0
 - initial version
--- lib/awshark/subcommands/cloud_formation.rb
+++ lib/awshark/subcommands/cloud_formation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'aws-sdk-cloudformation'
+require 'aws-sdk-ssm'
 require 'diffy'
 require 'recursive-open-struct'
 
